c0019318666b113080d294d9349b47cf9a58fd59
ChinthakaICIEOS <chinthaka@icieos.com>
2025-07-02 22:38:44 +0530

    two way binding ???

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,23 +1,28 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }){
+export default function Player({ initialName, symbol }){
+    const [playerName,setPlayerName] = useState(initialName);
     const [isEditing,setIsEditing] = useState(false);
 
     function handleEditeClick(){
-        // update state value based on previous value...
+        // update state value based on previous value... -> functional form...
         setIsEditing((editing) => !editing);
     }
 
-    let playerName = <span className="player-name">{name}</span>;
+    function handleChange(event){
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
 
     if(isEditing){
-        playerName = <input type="text" required value={name}/>
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>
     }
 
     return (
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
             <button type="button" onClick={handleEditeClick}>{isEditing ? "Save" : "Edit"}</button>
